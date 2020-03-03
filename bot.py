a6dfa9c549dd03309786c0d66e885df6eae0d366
mara880 <42419356+mara880@users.noreply.github.com>
2020-03-03 21:15:05 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,31 +1,42 @@
-from pynput.mouse import Listener
+from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Listener as KeyboardListener
+from pynput.keyboard import KeyCode
+from openpyxl import load_workbook
+from time import sleep as pause
+from tkinter import *
 import logging
 import pandas as pd
 import pyautogui
-from openpyxl import load_workbook
-from time import sleep
-from tkinter import *
 
+def mouse_monitor():
 
+# filemode - w - nadpisuje plik od początku
 
-def monitor_myszy():
-    # filemode - w - nadpisuje plik od początku
     logging.basicConfig(filename="mysz.txt",filemode="w" ,level=logging.DEBUG, format="%(message)s")
+    stop_key = KeyCode(char="s")
     def on_click(x,y, button, pressed):
         if pressed:
             logging.info("klik \t{0}\t {1}\t{2}".format(x, y, button))
 
-    def on_scroll(x, y):
-        logging.info("scroll \t{0}\t {1}".format(x, y))
+# akcja klawisza stopu
 
-    with Listener(on_click=on_click, on_scroll=on_scroll) as listener:
-        listener.join()
+    def on_press(key):
+        if key == stop_key:
+            listener.stop()
 
-def konwersja_logu_do_xls():
+# monitoring myszy i klawiatury jednoczesnie
+
+    with MouseListener(on_click=on_click) as listener:
+        with KeyboardListener(on_press=on_press) as listener:
+            listener.join()
+
+def convert_log_to_xls():
     log = pd.read_table("mysz.txt")
     log.to_excel("mysz.xlsx", index=False, header=True)
 
-def dodanie_wiersza_tytulowego():
+#dodanie wiersza z tytułem umozliwia uzycie funkcji loc biblioteki pandas, ktora traktuje pierwszy wiersz jako tytulowy
+
+def add_row_title():
     skoroszyt = load_workbook("mysz.xlsx")
     arkusz = skoroszyt.active
     arkusz.insert_rows(1)
@@ -35,29 +46,29 @@ def dodanie_wiersza_tytulowego():
     arkusz.cell(1,4,"KolumnaD")
     skoroszyt.save("mysz.xlsx")
 
-def odczyt_x():
+def read_x():
     x = pd.read_excel("mysz.xlsx")
     licznik = 0
-    ilosc_wierszy = x["KolumnaC"].size
+    ilosc_wierszy = x["KolumnaB"].size
     tablicax = []
     while licznik < ilosc_wierszy:
-        asd = x["KolumnaC"].loc[licznik]
+        asd = x["KolumnaB"].loc[licznik]
         licznik += 1
         tablicax.append(asd)
     return tablicax
 
-def odczyt_y():
+def read_y():
     y = pd.read_excel("mysz.xlsx")
     licznik = 0
-    ilosc_wierszy = y["KolumnaB"].size
+    ilosc_wierszy = y["KolumnaC"].size
     tablicay = []
     while licznik < ilosc_wierszy:
-        asd = y["KolumnaB"].loc[licznik]
+        asd = y["KolumnaC"].loc[licznik]
         licznik += 1
         tablicay.append(asd)
     return tablicay
 
-def odczyt_przycisku():
+def read_button():
     od = pd.read_excel("mysz.xlsx")
     licznik = 0
     ilosc_wierszy = od["KolumnaD"].size
@@ -69,37 +80,41 @@ def odczyt_przycisku():
         string_przycisku = string_przycisku.replace("Button.left", "left")
         string_przycisku = string_przycisku.replace("Button.right", "right")
         tablicaod.append(string_przycisku)
-
     return tablicaod
 
 # funkcja korzysta z odczytx i odczyty
 
 def start_bota():
-    konwersja_logu_do_xls()
-    dodanie_wiersza_tytulowego()
+    convert_log_to_xls()
+    add_row_title()
     licznikx = 0
     liczniky = 0
     licznikod = 0
     licznik_wykonania = pd.read_excel("mysz.xlsx")["KolumnaB"].size-1
     while licznik_wykonania > ile_razy_wykonac:
         while licznik_wykonania >= liczniky:
-            y = (odczyt_y()[liczniky])
-            x = (odczyt_x()[licznikx])
-            button = (odczyt_przycisku()[licznikod])
-            pyautogui.click(x,y, button=button)
+            y = (read_y()[liczniky])
+            x = (read_x()[licznikx])
+            button = (read_button()[licznikod])
+            pyautogui.click(x,y, button=button, interval=2)
             licznikx += 1
             liczniky += 1
-            sleep(2)
+            #pause(2)
 
+def main():
 
-window = Tk()
-window.title("Rejestrator bota")
-opcja1 = Button(window, text="Rejestrator bota", activeforeground= "#FF0000", command=monitor_myszy).grid(sticky=E+W, padx = 5, pady = 5)
-opcja2 = Button(window,text="Uruchom bota", activeforeground= "#FF0000", command=start_bota).grid(sticky=E+W, padx = 5, pady = 5)
-tytul = Label(window, text="Wybierz ile razy zostanie powtórzone zadanie:").grid(sticky=E+W, padx=5, pady=5)
-ilosc_powtorzen = Spinbox(window, from_=1, to=20).grid(sticky=E + W, padx=5, pady=5)
-opcja3 = Button(window, text="Zakończ").grid(sticky=E+W, padx = 5, pady = 5)
-ilosc_powtorzen = IntVar()
-ile_razy_wykonac = ilosc_powtorzen.get()
-window.mainloop()
+    window = Tk()
+    window.title("Rejestrator bota")
 
+    opcja1 = Button(window, text="Rejestrator bota", activeforeground= "#FF0000", command=mouse_monitor).grid(sticky=E+W, padx = 5, pady = 5)
+    opcja2 = Button(window,text="Uruchom bota", activeforeground= "#FF0000", command=start_bota).grid(sticky=E+W, padx = 5, pady = 5)
+    tytul = Label(window, text="Wybierz ile razy zostanie powtórzone zadanie:").grid(sticky=E+W, padx=5, pady=5)
+    ilosc_powtorzen = Spinbox(window, from_=1, to=20).grid(sticky=E + W, padx=5, pady=5)
+    opcja3 = Button(window, text="Zakończ", command=window.quit).grid(sticky=E+W, padx = 5, pady = 5)
+    ilosc_powtorzen = IntVar()
+    ile_razy_wykonac = ilosc_powtorzen.get()
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
